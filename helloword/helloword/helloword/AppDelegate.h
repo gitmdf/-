f055a4af9cac42a5a226f552122708d6b4fb1d1d
lanou3g <zhaoce@xiaowu.local>
2016-06-13 09:46:13 +0800

    sbno1

---
+++ helloword/helloword/helloword/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloword
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 fs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloword/helloword/helloword/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloword
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 fs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
